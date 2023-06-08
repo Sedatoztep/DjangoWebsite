65f8d9313c8a75fd0f031f727432300a69a2b179
Sedat Öztep <65872570+Sedatoztep@users.noreply.github.com>
2023-06-09 02:14:29 +0300

    Blogapp 2.1
    version 2.1

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,14 +1,47 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+data = {
+    "blogs": [
+        {
+            "id": 1,
+            "title": "About Python",
+            "image": "1.jpeg",
+            "is_active": True,
+            "is_home": False,
+            "description": "very good language"
+        },
+        {
+            "id": 2,
+            "title": "About Django",
+            "image": "2.png",
+            "is_active": True,
+            "is_home": True,
+            "description": "very good language"
+        },
+        {
+            "id": 3,
+            "title": "Cyber Security",
+            "image": "3.jpeg",
+            "is_active": False,
+            "is_home": True,
+            "description": "very good language"
+        },
+    ]
+}
 
 # Create your views here.
-
 def index(request):
-    return render(request,"blog/index.html")
+    context = {
+        "blogs": data["blogs"]
+    }
+    return render(request,"blog/index.html", context)
 
 def blogs(request):
-    return render(request,"blog/blogs.html")
+    context = {
+        "blogs": data["blogs"]
+    }
+    return render(request,"blog/blogs.html", context)
 
 def blog_details(request,id):
     return render(request, "blog/blog-details.html", {
