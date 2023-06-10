ae0086d3219484b9bb09d51d0f9ff4368c5eb000
Sedat Öztep <65872570+Sedatoztep@users.noreply.github.com>
2023-06-10 17:42:54 +0200

    Blogapp 2.5
    2.5

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,51 +1,31 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from blog.models import Blog
+from blog.models import Blog, Category
 
-data = {
-    "blogs": [
-        {
-            "id": 1,
-            "title": "About Python",
-            "image": "1.jpeg",
-            "is_active": True,
-            "is_home": False,
-            "description": "very good language"
-        },
-        {
-            "id": 2,
-            "title": "About Django",
-            "image": "2.png",
-            "is_active": True,
-            "is_home": True,
-            "description": "very good language"
-        },
-        {
-            "id": 3,
-            "title": "Cyber Security",
-            "image": "3.jpeg",
-            "is_active": False,
-            "is_home": True,
-            "description": "very good language"
-        },
-    ]
-}
-
-# Create your views here.
 def index(request):
     context = {
-        "blogs": Blog.objects.filter(is_active=True, is_home=True)
+        "blogs": Blog.objects.filter(is_active=True, is_home=True),
+        "categories": Category.objects.all()
     }
     return render(request,"blog/index.html", context)
 
 def blogs(request):
     context = {
-        "blogs": Blog.objects.filter(is_active=True)
+        "blogs": Blog.objects.filter(is_active=True),
+        "categories": Category.objects.all()
     }
     return render(request,"blog/blogs.html", context)
 
-def blog_details(request,id):
-   blog = Blog.objects.get(id=id)
+def blog_details(request,slug):
+   blog = Blog.objects.get(slug=slug)
    return render(request, "blog/blog-details.html", {
         "blog": blog
     })
+
+def blogs_by_category(request, slug):
+    context = {
+        "blogs": Blog.objects.filter(is_active=True, category__slug=slug),
+        "categories": Category.objects.all(),
+        "selected_category": slug
+    }
+    return render(request,"blog/blogs.html", context)
